c9b84828ffbb9e304f7e1e8d2de4fd39f519a84e
JoeBosi <bosi.giuseppe@gmail.com>
2026-03-04 14:28:50 +0100

    Before Tests

---
+++ Esegui.bat
@@ -0,0 +1,9 @@
+
+:: Spostati nella cartella dello script
+cd /d "G:\Il mio Drive\- Cursore\Atlassian-Ticket-Scraping"
+
+:: Esegui lo script (usa 'python' o 'python3' a seconda della tua installazione)
+python Atlassian-Ticket-Scraping.py
+
+:: Opzionale: decommenta la riga sotto se vuoi che la finestra resti aperta in caso di errore
+:: pause
